6cf157616989d65b6fdabb31511a97a9b9b8d8de
Drake <104961439+drake-ho-goldenowl@users.noreply.github.com>
2023-03-17 11:42:41 +0700

    change url

--- app/src/main/java/com/drake/demeapp/utils/Constants.kt
+++ app/src/main/java/com/drake/demeapp/utils/Constants.kt
@@ -1,14 +1,14 @@
 package com.drake.demeapp.utils
 
 val images = arrayListOf(
-    "https://github.com/drake-ho-goldenowl/DemeApp/blob/main/images/1.jpeg?raw=true",
-    "https://github.com/drake-ho-goldenowl/DemeApp/blob/main/images/2.jpeg?raw=true",
-    "https://github.com/drake-ho-goldenowl/DemeApp/blob/main/images/3.jpeg?raw=true",
-    "https://github.com/drake-ho-goldenowl/DemeApp/blob/main/images/4.jpeg?raw=true",
-    "https://github.com/drake-ho-goldenowl/DemeApp/blob/main/images/5.jpeg?raw=true",
-    "https://github.com/drake-ho-goldenowl/DemeApp/blob/main/images/6.jpeg?raw=true",
-    "https://github.com/drake-ho-goldenowl/DemeApp/blob/main/images/7.jpeg?raw=true",
-    "https://github.com/drake-ho-goldenowl/DemeApp/blob/main/images/8.jpeg?raw=true",
-    "https://github.com/drake-ho-goldenowl/DemeApp/blob/main/images/9.jpeg?raw=true",
-    "https://github.com/drake-ho-goldenowl/DemeApp/blob/main/images/10.jpeg?raw=true",
+    "https://github.com/drake-ho-goldenowl/DemeApp/blob/main/images/image_1.jpeg?raw=true",
+    "https://github.com/drake-ho-goldenowl/DemeApp/blob/main/images/image_2.jpeg?raw=true",
+    "https://github.com/drake-ho-goldenowl/DemeApp/blob/main/images/image_3.jpeg?raw=true",
+    "https://github.com/drake-ho-goldenowl/DemeApp/blob/main/images/image_4.jpeg?raw=true",
+    "https://github.com/drake-ho-goldenowl/DemeApp/blob/main/images/image_5.jpeg?raw=true",
+    "https://github.com/drake-ho-goldenowl/DemeApp/blob/main/images/image_6.jpeg?raw=true",
+    "https://github.com/drake-ho-goldenowl/DemeApp/blob/main/images/image_7.jpeg?raw=true",
+    "https://github.com/drake-ho-goldenowl/DemeApp/blob/main/images/image_8.jpeg?raw=true",
+    "https://github.com/drake-ho-goldenowl/DemeApp/blob/main/images/image_9.jpeg?raw=true",
+    "https://github.com/drake-ho-goldenowl/DemeApp/blob/main/images/image_10.jpeg?raw=true",
 )
